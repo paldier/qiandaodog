b26f3c788e729815b123234d7df21bb227efa739
Carseason <757841203@qq.com>
2019-03-31 20:38:59 +0800

    Update main.go

--- 2.0/main.go
+++ 2.0/main.go
@@ -18,6 +18,7 @@ func Openfile() (cookie []string) {
 	fi, err := os.Open("cookie.txt") //读取文件夹里的cookie.txt
 	if err != nil {
 		log.Println("No cookie.txt file")
+		os.Exit(0)
 	}
 	defer fi.Close()
 	rd := bufio.NewReader(fi)
@@ -30,133 +31,100 @@ func Openfile() (cookie []string) {
 	}
 	return
 }
-func StringSplitn(s1, s2 string) []string {
+func StringSplitn(s1, s2 string) ([]string, error) {
 	r1 := strings.SplitAfterN(s1, s2, 10)
+	if len(r1) != 2 {
+		return r1, fmt.Errorf("Array Not slice")
+	}
 	r1[1] = strings.Replace(r1[1], "\r", "", -1)
 	r1[1] = strings.Replace(r1[1], "\n", "", -1)
-	return r1
+	return r1, nil
 }
 func main() {
 	txtbody := Openfile()
 	t := len(txtbody)
 	for i := 0; i < t; i++ {
-		CookieBool := StringSplitn(txtbody[i], `"=`)
-		if CookieBool[1] == "" {
+		CookieBool, err := StringSplitn(txtbody[i], `"=`)
+		if err != nil || CookieBool[1] == "" {
 			continue
 		}
-		switch {
-		case CookieBool[0] == `"baidu"=`: //百度贴吧及文库
+		switch CookieBool[0] {
+		case `"baidu"=`: //百度贴吧及文库
 			c := &models.BaiduCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetBaidu()
-			break
-		case CookieBool[0] == `"v2ex"=`: //V2EX
+		case `"v2ex"=`: //V2EX
 			c := &models.V2exCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetV2ex()
-			break
-		case CookieBool[0] == `"hostloc"=`: //hostloc
+		case `"hostloc"=`: //hostloc
 			c := &models.HostlocCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetHostloc()
-			break
-
-		case CookieBool[0] == `"acfun"=`: //A站
+		case `"acfun"=`: //A站
 			c := &models.AcfunCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetAcfun()
-			break
-
-		case CookieBool[0] == `"bilibili"=`: //哔哩哔哩直播区
+		case `"bilibili"=`: //哔哩哔哩直播区
 			c := &models.BiliCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetBili()
-			break
-		case CookieBool[0] == `"163music"=`: //网易云音乐
+		case `"163music"=`: //网易云音乐
 			c := &models.Music163Cookie{}
 			c.Cookies = CookieBool[1]
 			c.SetMusic163()
-			break
-
-		case CookieBool[0] == `"miui"=`: //miui论坛
+		case `"miui"=`: //miui论坛
 			c := &models.MiuiCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetMiui()
-			break
-
-		case CookieBool[0] == `"52pojie"=`: //吾爱破解
+		case `"52pojie"=`: //吾爱破解
 			c := &models.Pojie52Cookie{}
 			c.Cookies = CookieBool[1]
 			c.SetPojie52()
-			break
-
-		case CookieBool[0] == `"kafan"=`: //卡饭
+		case `"kafan"=`: //卡饭
 			c := &models.KafanCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetKafan()
-			break
-
-		case CookieBool[0] == `"smzdm"=`: //什么值得买
+		case `"smzdm"=`: //什么值得买
 			c := &models.SmzdmCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetSmzdm()
-			break
-
-		case CookieBool[0] == `"jd"=`: //京东领钢镚
+		case `"jd"=`: //京东领钢镚
 			c := &models.JdCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetJd()
-			break
-
-		case CookieBool[0] == `"zimuzu"=`: //人人字幕组
+		case `"zimuzu"=`: //人人字幕组
 			c := &models.ZimuzuCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetZimuzu()
-			break
-		case CookieBool[0] == `"gztown"=`: //港知堂社区PT站
+		case `"gztown"=`: //港知堂社区PT站
 			c := &models.GztownCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetGztown()
-			break
-		case CookieBool[0] == `"meizu"=`: //魅族论坛
+		case `"meizu"=`: //魅族论坛
 			c := &models.MeizuCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetMeizu()
-			break
-
-		case CookieBool[0] == `"hdpfans"=`: //高清范论坛
+		case `"hdpfans"=`: //高清范论坛
 			c := &models.HdpdansCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetHdpdans()
-			break
-
-		case CookieBool[0] == `"chh"=`: //CHH
+		case `"chh"=`: //CHH
 			c := &models.ChhCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetChh()
-			break
-
-		case CookieBool[0] == `"koolshare"=`: //Koolshare
+		case `"koolshare"=`: //Koolshare
 			c := &models.KoolshareCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetKoolshare()
-			break
-
-		case CookieBool[0] == `"right"=`: //恩山
+		case `"right"=`: //恩山
 			c := &models.RightCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetRight()
-			break
-
-		case CookieBool[0] == `"huawei"=`: //花粉俱乐部
+		case `"huawei"=`: //花粉俱乐部
 			c := &models.HuaweiCookie{}
 			c.Cookies = CookieBool[1]
 			c.SetHuawei()
-			break
-
-		default:
-			break
-
 		}
 
 	}
